3558dbc51c0e571ca901f810da978e2bfda790be
윤설 <computer98400@gmail.com>
2021-12-06 23:25:39 +0900

    algorithm1206

--- algorithm1205/bj2206_notSolved.java
+++ algorithm1205/bj2206_notSolved.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
-public class bj2206 {
+//넘어간 사실과 넘어가기전, 넘어간 후 상태를 정해주면 된다.
+public class bj2206_notSolved {
     public static boolean[][] visited;
     public static boolean[][] copyVisited;
     public static int[][] copyMap;
@@ -47,7 +47,7 @@ public class bj2206 {
             }
         }
         for (int i = 0; i < walls.size(); i++) {
-            int[] broken = walls.get(i);
+            int[] broken = walls.get(i);    //각
             bfs(broken[0],broken[1]);
             for (int j = 0; j < map.length; j++) {
                 for (int j2 = 0; j2 < map[0].length; j2++) {
@@ -69,6 +69,24 @@ public class bj2206 {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0,0});        
         visited[0][0] = true;
+        //벽을 부순다라는 개념이 모든 경우에서 부서지는게 아니라, 해당 값만 부서지는것이다.
+        
+        // -> 0,0 0,1(벽, 이부분에서 상태가 변함.) 0,2
+        
+        // -> 0,0 1,0(벽이 아니라서 그냥 유지됨.) 2,0
+        
+        // map에 정보를 담음
+        
+        // 맵을 건드리지 않고 bfs를 돌리는 방식을 생각한다. -> 클래스형식으로 만드는게 좋을듯함.
+        
+        // 맵을 중간에 바꾸는경우는 플레문제임 아 ㅋㅋ
+
+        // visited의 경우는 공통 부분임.
+        
+        // visited는 업데이트가 되는 배열임.
+
+        // 
+
         map[0][0] = 1;
         while(!queue.isEmpty()){
             int[] pos = queue.poll();
