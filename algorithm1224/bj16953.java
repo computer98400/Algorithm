846e9352bc23c58d09d3d0acb8b69b1dd22960cd
윤설 <computer98400@gmail.com>
2021-12-24 14:41:46 +0900

    algorithm1224

---
+++ algorithm1224/bj16953.java
@@ -0,0 +1,62 @@
+package algorithm1224;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class bj16953 {
+    public static boolean show;
+    public static int solution;
+
+    static class check{
+        long N;
+        int count;
+
+        check(long N, int count) {
+            this.N = N;
+            this.count = count;
+        }
+    }
+    public static void bfs(long N, long M) {
+        Queue<check> que = new LinkedList<>();
+        que.offer(new check(N,1));
+        while (!que.isEmpty()) {
+            check pos = que.poll();
+
+            if (pos.N == M) {
+                show = true;
+                solution = pos.count;
+                return;
+            }
+            for (int i = 0; i < 2; i++) {
+                long temp = 0;
+                if (i == 0) {
+                    temp = 2 * pos.N;
+                } else {
+                    String temp2 = Long.toString(pos.N);
+                    temp2 += "1";
+                    temp = Integer.parseInt(temp2);
+                }
+                if (temp > M) {
+                    continue;
+                } else {
+                    que.offer(new check(temp,pos.count+1));
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        long N = sc.nextLong();
+        long M = sc.nextLong();
+
+        bfs(N, M);
+        if (show) {
+            System.out.println(solution);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
