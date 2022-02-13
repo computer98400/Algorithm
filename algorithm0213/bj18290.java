f1f7023ca561d39bcfd5bb557d71abe662ef6ea0
younseol <computer98400@gmail.com>
2022-02-13 23:43:58 +0900

    algorithm0213

---
+++ algorithm0213/bj18290.java
@@ -0,0 +1,29 @@
+package algorithm0213;
+
+import java.util.ArrayList;
+
+public class bj18290 {
+    public static int N;
+    public static int M;
+    public static int K;
+
+    public static void combi(int idx, ArrayList<int[]> solution) {
+        if (solution.size() == K) {
+
+            return;
+        }
+
+        for (int i = 0; i < N*M; i++) {
+            
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        
+
+
+    }
+}
