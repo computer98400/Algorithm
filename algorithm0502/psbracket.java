16890042a670485f38f690784d68f8dfd831354e
윤설 <computer98400@gmail.com>
2022-05-21 23:09:11 +0900

    algorithm0521

--- algorithm0502/psbracket.java
+++ algorithm0502/psbracket.java
@@ -3,51 +3,60 @@ package Algorithm.algorithm0502;
 import java.util.Stack;
 
 public class psbracket{
-    public static String total = "";
+
+    //재귀를 잘못함.
     public static String solution(String p) {
         if(p.equals("")){
             return "";
         }
-        
-        int test = 0;            
+    
+        StringBuilder u = new StringBuilder();
+        StringBuilder v = new StringBuilder();
+
+        int test = 0;         
         for(int i=0; i< p.length(); i++){
             if(p.charAt(i) == '('){
                 test++;
             }else{
                 test--;
             }
-
             if(test == 0){
-                String u = p.substring(0, i+1);
-                String v = p.substring(i+1);
-
-                //(0~i), (i+1~end)
-                if(verify(u)){
-                    total+= u;
-                    solution(v);
+                u.append(p.substring(0, i+1));
+                v.append(p.substring(i+1,p.length()));
+                
+                if(verify(u.toString())){        
+                    u.append( solution(v.toString()));
                 }else{
-                    String check1 = "";
-
+                    
+                    String temp = "";
+                    temp += '(';
+                    temp += solution(v.toString());
+                    temp+= ')';
+                    temp+= test(u.toString());
+                    return temp;
                 }
+                break;
             }
         }
-        return "";
+        return u.toString();
     }
 
-    public static void test(String temp){
-        String trans = "(";
-
-
-        return;
+    public static String test(String temp){
+        StringBuilder sb =new StringBuilder();
+        for(int i=1; i<temp.length()-1; i++){
+            if(temp.charAt(i) == '('){
+                sb.append(')');
+            }else{
+                sb.append('(');
+            }
+        }
+        return sb.toString();
     }
 
 
     public static boolean verify(String temp){
         Stack<Character> list = new Stack<>();
-
-
         for(int i =0; i<temp.length(); i++){
-
             //스택이 비어있을 경우 들어오는 값이 ')'라면 끝.
             if(temp.charAt(i) == ')'){
                 if(list.isEmpty()){
@@ -67,14 +76,8 @@ public class psbracket{
             return false;
         }
     }
-
-    public static String step(){
-        return "";
-    }
-
-
     public static void main(String[] args) {
-        solution("(()())()");
+        solution("()))((()");
     }
 
 
