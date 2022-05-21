683e7af4982ecf9ad7091b03a663047c16e5e08b
윤설 <computer98400@gmail.com>
2022-05-21 23:08:59 +0900

    algorithm0521

---
+++ algorithm0521/bj17298.java
@@ -0,0 +1,20 @@
+package Algorithm.algorithm0521;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj17298 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        
+    
+    
+    }
+}
